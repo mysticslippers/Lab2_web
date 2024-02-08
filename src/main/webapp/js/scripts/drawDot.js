e8d7db7454a8d82d4cbaae7fff50298d10ca020b
mysticslippers <eternal.martyr13579@gmail.com>
2024-02-08 13:13:30 +0300

    done

--- src/main/webapp/js/scripts/drawDot.js
+++ src/main/webapp/js/scripts/drawDot.js
@@ -9,7 +9,7 @@ function drawDot(x, y, r, result){
 
     {   //drawing dot
         ctx.beginPath();
-        ctx.arc(canvas.width / 2 + (step * x) * (r / 4), canvas.height / 2 + (step * -y) * (r / 4), 5, 0, 2 * Math.PI, false);
+        ctx.arc(canvas.width / 2 + (step * x) * (r / 5), canvas.height / 2 + (step * -y) * (r / 5), 5, 0, 2 * Math.PI, false);
         ctx.stroke();
         ctx.fill();
         ctx.closePath();
